11360f0924fbf6f59e5486e96ea2bf13ab319c74
Mattia Percassi <107413452+MattiaPercassi@users.noreply.github.com>
2022-09-21 20:10:25 +0900

    Changes in settings and main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <eigen/Eigen/Dense>
+#include <iostream>
+#include <fstream>
+#include "discretizeLTI.h"
+#include "augmentLTI.h"
+#include "globalMatrixes.h"
+
+int main()
+{
+
+    return 0;
+}
