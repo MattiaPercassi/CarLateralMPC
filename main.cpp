b9110d31b7a04ed8813693b3a19ba7784766583e
Mattia Percassi <107413452+MattiaPercassi@users.noreply.github.com>
2022-09-22 19:24:32 +0900

    All functions compile, not tested with actual data

--- main.cpp
+++ main.cpp
@@ -10,32 +10,39 @@
 int main()
 {
     // For simplicity matrixes dimentions are defined in initialization
-    constexpr int n = 20;       // prediction steps
-    constexpr int ec = 2;       // error vector size
-    constexpr int uc = 1;       // number of inputs
-    constexpr int sc = 5;       // number of states
-    constexpr int frac = 20;    // number of fractions for fine status calculation at each step
-    constexpr float h = 0.1;    // [s] integration interval, 1/sample frequency
-    constexpr int simTime = 10; // [s] total time of simulation
-    constexpr float m = 0;      //[kg] vehicle mass
-    constexpr float j = 0;      //[kg*m^2] moment if inertia
-    constexpr float caf = 0;    // [N] front wheel lateral force coefficient
-    constexpr float car = 0;    // [N] rear wheel lateral force coefficient
-    constexpr float lf = 0;     // [m] distance of center of mass and front wheel
-    constexpr float lr = 0;     // [m] distance of center of mass and rear wheel
-    constexpr float xd = 0;     // [m/s] longitudianl velocity
+    constexpr int n = 20;              // prediction steps
+    constexpr int ec = 2;              // error vector size
+    constexpr int uc = 1;              // number of inputs
+    constexpr int sc = 5;              // number of states
+    constexpr int frac = 20;           // number of fractions for fine status calculation at each step
+    constexpr float h = 0.1;           // [s] integration interval, 1/sample frequency
+    constexpr float smallh = h / frac; // [s] integration interval of the open loop system simulation
+    constexpr int simTime = 10;        // [s] total time of simulation
+    constexpr float m = 0;             //[kg] vehicle mass
+    constexpr float j = 0;             //[kg*m^2] moment if inertia
+    constexpr float caf = 0;           // [N] front wheel lateral force coefficient
+    constexpr float car = 0;           // [N] rear wheel lateral force coefficient
+    constexpr float lf = 0;            // [m] distance of center of mass and front wheel
+    constexpr float lr = 0;            // [m] distance of center of mass and rear wheel
+    constexpr float xd = 0;            // [m/s] longitudianl velocity
     // initial states vector
-    Eigen::Matrix<float, sc, 1> xk = Eigen::Matrix<float, sc, 1>::Zero();
+    Eigen::MatrixXf xk = Eigen::MatrixXf::Zero(sc, 1);
     // initialization input vector
-    Eigen::Matrix<float, n * uc, 1> ug = Eigen::Matrix<float, n * uc, 1>::Zero();
+    Eigen::MatrixXf ug = Eigen::MatrixXf::Zero(n * uc, 1);
     // initialization reference
-    Eigen::Matrix<float, ec, static_cast<int>(simTime / h)> ref = Eigen::Matrix<float, ec, static_cast<int>(simTime / h)>::Zero();
+    Eigen::MatrixXf ref = Eigen::MatrixXf::Zero(ec, simTime / h);
     // build matrix A
-    Eigen::Matrix<float, 4, 4> A = Eigen::Matrix<float, 4, 4>::Zero();
+    Eigen::MatrixXf A = Eigen::MatrixXf::Zero(4, 4);
     // build matrix B
-    Eigen::Matrix<float, 4, 1> B = Eigen::Matrix<float, 4, 1>::Zero();
-    // build matrix B
-    Eigen::Matrix<float, 2, 4> C = Eigen::Matrix<float, 2, 4>::Zero();
+    Eigen::MatrixXf B = Eigen::MatrixXf::Zero(4, 1);
+    // build matrix C
+    Eigen::MatrixXf C = Eigen::MatrixXf::Zero(2, 4);
+    // Build matrix Q
+    Eigen::MatrixXf Q = Eigen::MatrixXf::Zero(2, 2);
+    // Build matrix S
+    Eigen::MatrixXf S = Eigen::MatrixXf::Zero(2, 2);
+    // Build matrix R
+    Eigen::MatrixXf R = Eigen::MatrixXf::Zero(n * uc, uc);
 
     // discretize
     auto Ad = discretizeA(A, h);
@@ -48,7 +55,29 @@ int main()
     auto Ct = augmentC(Cd);
 
     // global matrixes
-    Eigen::Matrix<float, n * sc, sc> Ag = buildGlobalA<n, sc>(At);
+    auto Ag = buildGlobalAdyn(At, n);
+    auto Bg = buildGlobalBdyn(At, Bt, n, sc, uc);
+    auto Qg = buildGlobalQdyn(Q, S, Ct, n, sc, ec);
+    auto Tg = buildGlobalTdyn(Q, S, Ct, n, sc, ec);
+    auto Rg = buildGlobalRdyn(R, n, uc);
+
+    // gradient matrixes
+    auto invH = buildinverseHdyn(Bg, Qg, Rg);
+    auto F = buildFdyn(Ag, Qg, Bg, Tg, n, sc, ec, uc);
+
+    // steering angle
+    float delta{0};
+
+    for (float t{0}; t < simTime / h; t += h)
+    {
+        ug = calculateOptInputsdyn(invH, F, xk, refg, n, sc, ec);
+        delta += ug.block(0, 0, uc, 1);
+        for (int j{0}; j < frac; ++j)
+        {
+            // calculate the open loop system status until the next time step
+        };
+        // write to file
+    };
 
     return 0;
 };
